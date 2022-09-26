739f2deda98d655f9d547069c47d705c4dafd998
poerlang <30558209@qq.com>
2022-09-26 20:38:35 +0800

    adjust width

--- src/main/java/com/poerlang/nars3dview/GUI.java
+++ src/main/java/com/poerlang/nars3dview/GUI.java
@@ -177,7 +177,7 @@ public class GUI extends Application {
         //第二个窗口，用来设置线宽、颜色等：
         ImGui.setNextWindowPos(windowAPos.x,windowAPos.y+windowASize.y+20);
         ImGui.begin("3D View Setting", ImGuiWindowFlags.AlwaysAutoResize);
-        ImGui.sliderFloat("Line3d Width", ViewSetting.lineSetting.lineWidth, 0, 1);
+        ImGui.sliderFloat("Line3d Width", ViewSetting.lineSetting.lineWidth, 0, 5);
         ImGui.text("Line3d Normal Color:");
         ImGui.colorEdit4("Normal Start Point", ViewSetting.lineSetting.normalStartColor.data);
         ImGui.colorEdit4("Normal End Point", ViewSetting.lineSetting.normalEndColor.data);

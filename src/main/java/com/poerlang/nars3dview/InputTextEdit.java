f15bc3ffafb2c9b4702e25e28ea37b6fd24dfe2e
poerlang <30558209@qq.com>
2022-09-29 21:19:12 +0800

    fix a ui crack (miss draw end)

--- src/main/java/com/poerlang/nars3dview/InputTextEdit.java
+++ src/main/java/com/poerlang/nars3dview/InputTextEdit.java
@@ -39,12 +39,12 @@ public class InputTextEdit {
         lang.setPreprocIdentifiers(preprocIdentifierMap);
 
         String[] identifiers = {
-            "-->",
-            "==>",
+                "-->",
+                "==>",
         };
         String[] idecls = {
-            "inheritance",
-            "implication",
+                "inheritance",
+                "implication",
         };
 
         // Adding custom identifiers
@@ -62,101 +62,100 @@ public class InputTextEdit {
         EDITOR.setErrorMarkers(errorMarkers);
 
         EDITOR.setPalette(new int[]{
-            0xff7f7f7f,	// Default
-            0xffd69c56,	// Keyword
-            0xff00ff00,	// Number
-            0xff7070e0,	// String
-            0xff70a0e0, // Char literal
-            0xffffffff, // Punctuation
-            0xff408080,	// Preprocessor
-            0xffaaaaaa, // Identifier
-            0xff9bc64d, // Known identifier
-            0xffc040a0, // Preproc identifier
-            0xffffffff, // Comment (single line)
-            0xffffffff, // Comment (multi line)
-            0xff101010, // Background
-            0xffe0e0e0, // Cursor
-            0x80a06020, // Selection
-            0x800020ff, // ErrorMarker
-            0x40f08000, // Breakpoint
-            0xff707000, // Line number
-            0x40000000, // Current line fill
-            0x40808080, // Current line fill (inactive)
-            0x40a0a0a0, // Current line edge
+                0xff7f7f7f,    // Default
+                0xffd69c56,    // Keyword
+                0xff00ff00,    // Number
+                0xff7070e0,    // String
+                0xff70a0e0, // Char literal
+                0xffffffff, // Punctuation
+                0xff408080,    // Preprocessor
+                0xffaaaaaa, // Identifier
+                0xff9bc64d, // Known identifier
+                0xffc040a0, // Preproc identifier
+                0xffffffff, // Comment (single line)
+                0xffffffff, // Comment (multi line)
+                0xff101010, // Background
+                0xffe0e0e0, // Cursor
+                0x80a06020, // Selection
+                0x800020ff, // ErrorMarker
+                0x40f08000, // Breakpoint
+                0xff707000, // Line number
+                0x40000000, // Current line fill
+                0x40808080, // Current line fill (inactive)
+                0x40a0a0a0, // Current line edge
         });
         EDITOR.setTextLines(new String[]{
-            "",
-            "",
-            "<swan --> swimmer>. %0.90%",
-            "<swan --> bird>.",
-            "<小特 --> 鸟>.",
-            "<鸟 --> 动物>.",
-            "",
-            "",
+                "",
+                "",
+                "<swan --> swimmer>. %0.90%",
+                "<swan --> bird>.",
+                "<小特 --> 鸟>.",
+                "<鸟 --> 动物>.",
+                "",
+                "",
         });
     }
 
     public static void show() {
-        if(nar == null)
+        if (nar == null)
             return;
 
         int inputEditorWidth = 300;
         imgui.internal.ImGui.setNextWindowSize(inputEditorWidth, 376, ImGuiCond.Once);
         ImGuiViewport imGuiViewport = imgui.internal.ImGui.getMainViewport();
-        imgui.internal.ImGui.setNextWindowPos(imGuiViewport.getPosX()+imGuiViewport.getSizeX()  - inputEditorWidth - 10, imGuiViewport.getPosY() + 10, ImGuiCond.FirstUseEver);
-        if (ImGui.begin("Input Editor",
-                ImGuiWindowFlags.HorizontalScrollbar | ImGuiWindowFlags.MenuBar)) {
-            if (ImGui.beginMenuBar()) {
-                if (ImGui.beginMenu("Edit")) {
+        imgui.internal.ImGui.setNextWindowPos(imGuiViewport.getPosX() + imGuiViewport.getSizeX() - inputEditorWidth - 10, imGuiViewport.getPosY() + 10, ImGuiCond.FirstUseEver);
+        ImGui.begin("Input Editor", ImGuiWindowFlags.HorizontalScrollbar | ImGuiWindowFlags.MenuBar);
+        if (ImGui.beginMenuBar()) {
+            if (ImGui.beginMenu("Edit")) {
 
-                    if (ImGui.menuItem("Undo", "ALT-Backspace", EDITOR.canUndo())) {
-                        EDITOR.undo(1);
-                    }
-                    if (ImGui.menuItem("Redo", "Ctrl-Y", EDITOR.canRedo())) {
-                        EDITOR.redo(1);
-                    }
-
-                    ImGui.separator();
-
-                    if (ImGui.menuItem("Copy", "Ctrl-C", EDITOR.hasSelection())) {
-                        EDITOR.copy();
-                    }
-                    if (ImGui.menuItem("Cut", "Ctrl-X", EDITOR.hasSelection())) {
-                        EDITOR.cut();
-                    }
-                    if (ImGui.menuItem("Delete", "Del", EDITOR.hasSelection())) {
-                        EDITOR.delete();
-                    }
-                    if (ImGui.menuItem("Paste", "Ctrl-V", ImGui.getClipboardText() != null)) {
-                        EDITOR.paste();
-                    }
-
-                    ImGui.endMenu();
+                if (ImGui.menuItem("Undo", "ALT-Backspace", EDITOR.canUndo())) {
+                    EDITOR.undo(1);
                 }
-                if (ImGui.beginMenu("Run")) {
-                    if (ImGui.menuItem("Add Text To NARS Input")) {
-                        play();
-                    }
-                    ImGui.endMenu();
+                if (ImGui.menuItem("Redo", "Ctrl-Y", EDITOR.canRedo())) {
+                    EDITOR.redo(1);
                 }
 
-                ImGui.endMenuBar();
+                ImGui.separator();
+
+                if (ImGui.menuItem("Copy", "Ctrl-C", EDITOR.hasSelection())) {
+                    EDITOR.copy();
+                }
+                if (ImGui.menuItem("Cut", "Ctrl-X", EDITOR.hasSelection())) {
+                    EDITOR.cut();
+                }
+                if (ImGui.menuItem("Delete", "Del", EDITOR.hasSelection())) {
+                    EDITOR.delete();
+                }
+                if (ImGui.menuItem("Paste", "Ctrl-V", ImGui.getClipboardText() != null)) {
+                    EDITOR.paste();
+                }
+
+                ImGui.endMenu();
+            }
+            if (ImGui.beginMenu("Run")) {
+                if (ImGui.menuItem("Add Text To NARS Input")) {
+                    play();
+                }
+                ImGui.endMenu();
             }
 
-            ImGui.text("Cycle After Input:");
-            ImGui.sliderInt(" ", Settings.narsSetting.cycleAfterInput.getData(), 0,50);
-            ImGui.separator();
-
-            if(ImGui.arrowButton("Play", ImGuiMouseButton.Right)){
-                play();
-            }
-            ImGui.sameLine(); ImGui.text("Send Text To NARS Channel");
-            ImGui.separator();
-
-            EDITOR.render("TextEditor");
-            MainGame.imGuiHover = ImGui.isAnyItemHovered() || ImGui.isWindowHovered();
-            ImGui.end();
+            ImGui.endMenuBar();
         }
+
+        ImGui.text("Cycle After Input:");
+        ImGui.sliderInt(" ", Settings.narsSetting.cycleAfterInput.getData(), 0, 50);
+        ImGui.separator();
+
+        if (ImGui.arrowButton("Play", ImGuiMouseButton.Right)) {
+            play();
+        }
+        ImGui.sameLine();
+        ImGui.text("Send Text To NARS Channel");
+        ImGui.separator();
+
+        MainGame.imGuiHover = ImGui.isAnyItemHovered() || ImGui.isWindowHovered();
+        EDITOR.render("TextEditor");
+        ImGui.end();
     }
 
     private static void play() {
@@ -167,7 +166,7 @@ public class InputTextEdit {
                 try {
                     nar.addInput(line);
                     nar.cycles(Settings.narsSetting.cycleAfterInput.get());
-                    if(!Settings.renderSetting.AutoRender.get()) View3dRefresh.refresh3DView();
+                    if (!Settings.renderSetting.AutoRender.get()) View3dRefresh.refresh3DView();
                 } catch (Exception ex) {
                     if (Debug.DETAILED) {
                         throw new IllegalStateException("error parsing:" + line, ex);
